5c96ded58fe3fc80efbfae0044e747e3c14d8fc5
Matheussvini <matheussvini@outlook.com>
2022-08-22 07:59:35 -0300

    Layou do jogo criado, cartas adicionadas

---
+++ JS/scripts.js
@@ -0,0 +1,21 @@
+const cards =[    
+    "./gifs/3parrots.gif",
+    "./gifs/birthdayparrot.gif",
+    "./gifs/brazilparrot.gif",
+    "./gifs/detectiveparrot.gif",
+    "./gifs/devilparrot.gif",
+    "./gifs/dogeparrot.gif",
+    "./gifs/drinkingparrot.gif",
+    "./gifs/footballparrot.gif",
+    "./gifs/headbangingparrot.gif",
+    "./gifs/lsdparrot.gif",
+    "./gifs/mustacheparrot.gif",
+    "./gifs/negativeparrot.gif",
+    "./gifs/pirateparrot.gif",
+    "./gifs/policeparrot.gif",
+    "./gifs/popcornparrot.gif",
+    "./gifs/scientistparrot.gif",
+    "./gifs/soccerparrot.gif",
+    "./gifs/sunglassparrot.gif",
+    "./gifs/tenisparrot.gif"
+]
