bc79fa756638e854856f98bf63b72061416fe788
Matheussvini <matheussvini@outlook.com>
2022-08-22 09:47:09 -0300

    Função adicionar cartas e embaralhar adicionadas

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,4 @@
-const cards =[    
+const gifs =[    
     "./gifs/3parrots.gif",
     "./gifs/birthdayparrot.gif",
     "./gifs/brazilparrot.gif",
@@ -18,4 +18,47 @@ const cards =[
     "./gifs/soccerparrot.gif",
     "./gifs/sunglassparrot.gif",
     "./gifs/tenisparrot.gif"
-]
+];
+
+const card = [];
+
+let qtdeCards = prompt('Com quantas cartas você deseja jogar? Somente números pares de 4 a 14');
+
+while(qtdeCards < 4 || qtdeCards > 14 || qtdeCards % 2 !== 0 ){
+    qtdeCards = prompt('Com quantas cartas você deseja jogar? Somente números pares de 4 a 14');
+};
+
+function shuffleCards(){
+    gifs.sort(comparator);
+    for ( let i = 0; i < qtdeCards/2; i++){        
+        card.push(gifs[i]);
+        card.push(gifs[i]);
+    }
+
+    card.sort(comparator);
+};
+
+function comparator() { 
+	return Math.random() - 0.5; 
+};
+
+shuffleCards();
+
+function insertCards(){
+    const game = document.querySelector('.game');
+     game.innerHTML = "";
+
+     for ( let i = 0; i < qtdeCards; i++){
+        game.innerHTML += `
+        <div class="card">
+            <div class="front">
+                <img src="./images/front.png" alt="Parrot image">                
+            </div>
+            <div class="back"><img src="${card[i]}" alt=""></div>
+        </div>
+        `;
+     }
+};
+
+insertCards();
+
